65b9bc669c17c96368254eb543cac7e3fcf24e63
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-10-01 18:21:05 +0100

    feat: modify article_controller.rb add functions destroy &  update

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -17,7 +17,23 @@ module Api
         if article.save
           render json: {status: 'SUCCESS', message:'Saved article', data:article}, status: :ok
         else
-          render json: {status: 'ERROR', message:'Article not saved', data:article.errors}, status: :unprocessable_entry
+          render json: {status: 'ERROR', message:'Article not saved', data:article.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        article = Article.find(params[:id])
+        article.destroy
+        render json: {status: 'SUCCESS', message:'Deleted article', data:article}, status: :ok
+      end
+
+      def update
+        article = Article.find(params[:id])
+        if article.update_attributes(article_params)
+          render json: {status: 'SUCCESS', message:'Updated article', data:article}, status: :ok
+        else
+            render json: {status: 'ERROR', message:'Article not updated',
+            data:article.errors}, status: :unprocessable_entity
         end
       end
 
@@ -26,7 +42,7 @@ module Api
       def article_params
         params.permit(:title, :body)
       end
-      
+
     end
   end
 end
